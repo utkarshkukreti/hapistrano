34ac7377b19b694acf227263754a25faabb7d48b
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-09-26 21:53:40 +0530

    Add deploy user, and add it to sudoers without password.

--- lib/hapistrano/bootstrap.rb
+++ lib/hapistrano/bootstrap.rb
@@ -2,12 +2,19 @@ module Hapistrano
   class Bootstrap
     def initialize(host)
       begin
-        ssh = Net::SSH.start(host, 'root')
+        @ssh = Net::SSH.start(host, 'root')
       rescue 
         password = HighLine.new.ask("Enter password: ") { |q| q.echo = '' }
-        ssh = Net::SSH.start(host, 'root', password: password)
+        @ssh = Net::SSH.start(host, 'root', password: password)
       end
-      puts ssh.exec! "whoami"
+      exec 'whoami'
+      exec "useradd -m -d /var/apps/ deploy"
+      exec "echo 'deploy ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
+    end
+
+    def exec(cmd)
+      puts "*** Executing #{cmd}"
+      puts @ssh.exec!(cmd)
     end
   end
 end
