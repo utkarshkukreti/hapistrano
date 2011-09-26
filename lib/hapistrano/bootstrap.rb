52f9dc3c7c579be6622f0a6925891c8e23867fbd
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-09-26 22:33:35 +0530

    Copy ssh key and close connection. Then login as deploy.

--- lib/hapistrano/bootstrap.rb
+++ lib/hapistrano/bootstrap.rb
@@ -10,11 +10,25 @@ module Hapistrano
       exec 'whoami'
       exec "useradd -m -d /var/apps/ deploy"
       exec "echo 'deploy ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
+
+      exec "mkdir /var/apps/.ssh"
+      ssh_key = File.read(File.expand_path("~/.ssh/id_rsa.pub"))
+      exec "echo #{esc ssh_key} >> /var/apps/.ssh/authorized_keys"
+      @ssh.close
+
+      @ssh = Net::SSH.start(host, 'deploy')
+      exec 'whoami'
     end
 
+    private
     def exec(cmd)
       puts "*** Executing #{cmd}"
       puts @ssh.exec!(cmd)
+    end
+
+    def esc(cmd)
+      return "''" if cmd.empty? 
+      cmd.gsub(/([^A-Za-z0-9_\-.,:\/@\n])/n, "\\\\\\1").gsub(/\n/, "'\n'")
     end
   end
 end
