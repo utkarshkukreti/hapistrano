af0b786961839afc374119658cbce89afc7d8e71
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-10-03 20:08:37 +0530

    Switch to rvm. Works until passenger installation.

--- lib/hapistrano/bootstrap.rb
+++ lib/hapistrano/bootstrap.rb
@@ -8,7 +8,7 @@ module Hapistrano
         @ssh = Net::SSH.start(host, 'root', password: password)
       end
       exec 'whoami'
-      exec "useradd -m -d /var/apps/ deploy"
+      exec "useradd -m -d /var/apps/ --shell /bin/bash deploy"
       exec "echo 'deploy ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
 
       exec "mkdir /var/apps/.ssh"
@@ -21,12 +21,16 @@ module Hapistrano
       exec 'sudo apt-get update'
       exec 'sudo apt-get install -y git-core build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev libcurl4-openssl-dev'
 
-      exec 'git clone https://github.com/sstephenson/ruby-build.git'
-      exec 'cd ruby-build && sudo ./install.sh'
-      exec 'sudo ruby-build 1.9.2-p290 /usr/local'
+      exec 'bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)'
+      exec 'cp ~/.bashrc ~/.bashrc_tmp && echo "[[ -s \"$HOME/.rvm/scripts/rvm\" ]] && source \"$HOME/.rvm/scripts/rvm\"" > ~/.bashrc && cat ~/.bashrc_tmp >> ~/.bashrc && rm ~/.bashrc_tmp'
+      exec '. ~/.bashrc'
+
+      exec 'type rvm'
+      exec 'rvm install 1.9.2'
+      exec 'rvm --default use 1.9.2'
 
       exec 'gem install passenger'
-      exec 'passenger-install-nginx-module --auto --auto-download --prefix="/opt/nginx"'
+      exec 'rvmsudo passenger-install-nginx-module --auto --auto-download --prefix="/etc/nginx"'
     end
 
     private
