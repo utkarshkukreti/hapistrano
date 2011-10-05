815445ab8c72edec7231956bd2f8ffd168381dc2
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-10-05 12:40:21 +0530

    Postgresql *just* works.

--- lib/hapistrano/bootstrap.rb
+++ lib/hapistrano/bootstrap.rb
@@ -1,34 +1,44 @@
 module Hapistrano
   class Bootstrap
-    def initialize(host)
+    def initialize(argv)
+      host = argv.shift
+      @verbose = argv.shift
       begin
         @ssh = Net::SSH.start(host, 'root')
       rescue 
         password = HighLine.new.ask("Enter password: ") { |q| q.echo = '' }
         @ssh = Net::SSH.start(host, 'root', password: password)
       end
+
+      log "Creating user deploy"
       exec 'whoami'
       exec "useradd -m -d /var/apps/ --shell /bin/bash deploy"
       exec "echo 'deploy ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
 
+      log "Adding your ssh key for user deploy"
       exec "mkdir /var/apps/.ssh"
       ssh_key = File.read(File.expand_path("~/.ssh/id_rsa.pub"))
       exec "echo #{esc ssh_key} >> /var/apps/.ssh/authorized_keys"
       @ssh.close
 
+      log "Logging in as user deploy"
       @ssh = Net::SSH.start(host, 'deploy')
       exec 'whoami'
+
+      log "Installing dependencies"
       exec 'sudo apt-get update'
       exec 'sudo apt-get install -y git-core build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev libcurl4-openssl-dev'
 
+      log "Installing RVM"
       exec 'bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)'
       exec 'cp ~/.bashrc ~/.bashrc_tmp && echo "[[ -s \"$HOME/.rvm/scripts/rvm\" ]] && source \"$HOME/.rvm/scripts/rvm\"" > ~/.bashrc && cat ~/.bashrc_tmp >> ~/.bashrc && rm ~/.bashrc_tmp'
       exec '. ~/.bashrc'
 
-      exec 'type rvm'
+      log "Installing ruby 1.9.2"
       exec 'rvm install 1.9.2'
       exec 'rvm --default use 1.9.2'
 
+      log "Installing passenger with nginx"
       exec 'gem install passenger'
       exec 'rvmsudo passenger-install-nginx-module --auto --auto-download --prefix="/opt/nginx"'
 
@@ -39,18 +49,22 @@ module Hapistrano
       exec 'sudo /usr/sbin/update-rc.d -f nginx defaults'
       exec 'sudo service nginx restart'
 
-      exec 'sudo apt-get install postgresql'
+      log "Installing postgresql"
+      exec 'sudo apt-get install -y postgresql-8.4'
+
       # Use utf-8
       exec 'sudo pg_dropcluster --stop 8.4 main'
       exec 'sudo pg_createcluster --start -e UTF-8 8.4 main'
       exec 'sudo /etc/init.d/postgresql-8.4 restart'
+      exec 'sudo su -c "createuser -rds deploy" postgres'
+      exec 'sudo su -c "createdb hello_world" postgres'
     end
 
     private
     def exec(cmd)
       puts "*** Executing #{cmd}"
       @ssh.exec!(cmd) do |channel, stream, data|
-        puts data
+        puts data if @verbose
       end
     end
 
--- lib/hapistrano/cli.rb
+++ lib/hapistrano/cli.rb
@@ -1,8 +1,8 @@
 module Hapistrano
   class CLI
     def initialize(argv)
-      if argv.first == "bootstrap" && argv.length == 2
-        Bootstrap.new(argv[1])
+      if argv.first == "bootstrap" && argv.length >= 2
+        Bootstrap.new(argv)
       else
         puts "You sent #{argv}"
       end
